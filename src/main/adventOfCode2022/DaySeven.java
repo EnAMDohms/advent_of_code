a22bac9569516dca9f99140ad9ce1139ac8680d8
nDohms <n_dohms@web.de>
2022-12-17 03:20:53 +0100

    Day seven part 1

---
+++ src/main/adventOfCode2022/DaySeven.java
@@ -0,0 +1,65 @@
+package main.adventOfCode2022;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import main.adventOfCode2022.dataModel.DaySeven.Directory;
+
+public class DaySeven {
+
+    public static void main(String[] args) {
+        // String inputPath = "resources/test.txt";
+        String inputPath = "resources/DaySeven.txt";
+
+        try {
+            List<String> commands = Files.readAllLines(Paths.get(inputPath));
+
+            partOne(commands);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void partOne(List<String> commands) {
+        Directory rootDirectory = new Directory("/");
+        Directory activeDirectory = rootDirectory;
+        for (String command : commands) {
+            if (command.equals("$ cd /")) {
+                activeDirectory = rootDirectory;
+            } else if (command.startsWith("$ cd ")) {
+                activeDirectory = activeDirectory.changeDirectory(command.substring(5));
+            } else if (command.matches("\\d+\\s[\\w\\W]+")) {
+                String[] file = command.split(" ");
+                activeDirectory.addFile(file[1], Integer.parseInt(file[0]));
+            } else if (command.startsWith("dir ")) {
+                activeDirectory.addSubdirectory(activeDirectory, command.substring(4));
+            }
+        }
+
+        System.out.println("result: " + sumOfSmallSizeDirectories(rootDirectory));
+    }
+
+    private static int sumOfSmallSizeDirectories(Directory directory) {
+        List<Integer> directorySizes = new ArrayList();
+        populateDirctorySizeMap(directory, directorySizes);
+
+        int sum = 0;
+        for (int directorySize : directorySizes) {
+            sum += directorySize;
+        }
+        return sum;
+    }
+
+    private static void populateDirctorySizeMap(Directory directory, List<Integer> directorySizes) {
+        for (Directory subdirectory : directory.getSubDirectories()) {
+            populateDirctorySizeMap(subdirectory, directorySizes);
+        }
+        if (directory.getSize() <= 100000) {
+            directorySizes.add(directory.getSize());
+        }
+    }
+}
---
+++ src/main/adventOfCode2022/dataModel/DaySeven/Directory.java
@@ -0,0 +1,108 @@
+package main.adventOfCode2022.dataModel.DaySeven;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Directory {
+
+    private List<Directory> subDirectories = new ArrayList<>();
+    private Directory parentDirectory;
+    private HashMap<String, Integer> files = new HashMap<>();
+    private String name = "";
+    private int size = 0;
+
+    public Directory() {
+    }
+
+    public Directory(String name) {
+        this.name = name;
+        parentDirectory = this;
+    }
+
+    public Directory(Directory parentDirectory, String name) {
+        this.name = name;
+        this.parentDirectory = parentDirectory;
+    }
+
+    public Directory changeDirectory(String directoryName) {
+        if (directoryName.equals("..")) {
+            return parentDirectory;
+        } else {
+            return openSubDirectoryByName(directoryName);
+        }
+    }
+
+    public Directory openSubDirectoryByName(String directoryName) {
+
+        for (Directory subDirectory : subDirectories) {
+            if (subDirectory.getName().equals(directoryName)) {
+                return subDirectory;
+            }
+        }
+
+        return null;
+    }
+
+    public void addFile(String fileName, int fileSize) {
+        files.put(fileName, fileSize);
+        addSize(fileSize);
+    }
+
+    public void addSize(int fileSize) {
+        this.size += fileSize;
+        if (!this.name.equals("/")) {
+            parentDirectory.addSize(fileSize);
+        }
+    }
+
+    public List<Directory> getSubDirectories() {
+        return subDirectories;
+    }
+
+    public void setSubDirectories(List<Directory> subDirectories) {
+        this.subDirectories = subDirectories;
+    }
+
+    public HashMap<String, Integer> getFiles() {
+        return files;
+    }
+
+    public void setFiles(HashMap<String, Integer> files) {
+        this.files = files;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public void addSubdirectory(Directory parentDirectory, String directoryName) {
+
+        if (!hasSubdirectorty(directoryName)) {
+            subDirectories.add(new Directory(parentDirectory, directoryName));
+        }
+
+    }
+
+    public boolean hasSubdirectorty(String directoryName) {
+        for (Directory subDirectory : this.subDirectories) {
+            if (subDirectory.getName().equals(directoryName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
